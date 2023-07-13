6e15d0216fce978aff7d28d31009bb2fa604faf1
Ayuns <116968584+Ayunitaputri@users.noreply.github.com>
2023-07-13 21:40:24 +0700

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,39 @@
+const {
+  // eslint-disable-next-line max-len
+  addItemHandler, getAllItemsHandler, getItemByIdHandler, editItemByIdHandler, deleteItemByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addItemHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllItemsHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getItemByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editItemByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteItemByIdHandler,
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: () => 'Halaman tidak dapat ditemukan',
+  },
+];
+
+module.exports = routes;
